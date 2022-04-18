279301ba059487426307566cd989bbe42357885f
root <root@andy9893.localdomain>
2022-04-18 09:35:32 +0530

    mysql connection function code is writed

--- User.go
+++ User.go
@@ -1,9 +1,39 @@
 package login_signup
 
 import (
-	"fmt"
+        "encoding/hex"
+        "golang.org/x/crypto/bcrypt"
+        "math/rand"
 )
+// This structure is used to store user details
+type User struct {
+	Name string `json:"user_name"`
+	Password string `json:"password"`
+	Organization string `json:"organization_name"`
+	Email string `json:"email"`
+	Auth_token string `json:"auth_token"`
+	Mobile string `json:"mobile"`
+	Note string `json:"note"`
+	Pin string `json:"pin"`
+	Object_token string `json:"object_token"`
+}
 
-func Test(){
-	fmt.Println("Testing Files");
+
+func EncryptPassword(password string) (string,error) {
+	bytes,err := bcrypt.GenerateFromPassword([]byte(password),14);
+	return string(bytes),err;
+}
+
+func CheckEqualPassword(password,hashPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash),[]byte(password));
+	return err==nil;
+}
+
+
+func GenerateToken(length int) string {
+    b := make([]byte, length)
+    if _, err := rand.Read(b); err != nil {
+        return ""
+    }
+    return hex.EncodeToString(b)
 }
---
+++ database.go
@@ -0,0 +1,46 @@
+package login_signup
+
+import (
+	"fmt"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Database struct {
+	SERVER_NAME      string  `json:"server_name"` // dns url
+	SERVER_USER_NAME string  `json:"server_user_name"` // server name like root
+	SERVER_PASSWORD  string  `json:"server_password"` // password for db
+	DATABASE_NAME    string  `json:"database_name"` // which database to use
+	DB               *sql.DB `json:"database_object"` // object to acess db
+	ERROR		 error   `json:"error"` //error if occured
+	DNS_URL		 string  `json:"dns_url"`
+}
+
+func DNSstring(url,user,password,database string) string {
+	dns := fmt.Sprintf("%s:%s@tcp(%s)/%s",user,password,url,database);
+	return dns;
+}
+
+func CreateDatabaseObject(server,user,password,database string) *Database{
+	var obj Database
+	obj.SERVER_NAME      = server;
+	obj.SERVER_USER_NAME = user;
+	obj.SERVER_PASSWORD  = password;
+	obj.DATABASE_NAME    = database;
+	obj.DNS_URL	     = DNSstring(server,user,password,database);
+	return &obj;
+}
+
+
+func ConnectDB(server,url,user,password,database string) *Database{
+	obj := CreateDatabaseObject(url,user,password,database);
+	if(server == "mysql"){
+		obj.DB,obj.ERROR = sql.Open(server,obj.DNS_URL);
+	}
+	return &obj;
+}
+
+
+
+
+
